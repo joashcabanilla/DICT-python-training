073cea0df17ecb526223fc976dfd2cb8a6c4af04
joascabanilla <joash0199@gmail.com>
2021-08-02 16:14:22 +0800

    day1

---
+++ Exer6.py
@@ -0,0 +1,29 @@
+# #upper() function
+# msg1 = "The quick brown fox jumps over a lazy dog"
+# print(msg1.upper())
+#
+# #lower() function
+# msg2 = "The quick brown fox jumps over a lazy dog"
+# print(msg2.lower())
+#
+# #capitalize() function
+# msg3 = "The quick brown fox jumps over a lazy dog"
+# print(msg3.capitalize())
+#
+# #title() function
+# msg4 = "The quick brown fox jumps over a lazy dog"
+# print(msg4.title())
+#
+# #len() function
+# msg5 = "The quick brown fox jumps over a lazy dog"
+# print(len(msg5))
+#
+# #count() function
+# list = ["apple", "grapes", "banana", "apple", "banana", "kiwi", "orange"]
+# print(list.count("apple"))
+
+#replace() function
+msg6 = "The quick brown fox jumps over a lazy dog"
+res = msg6.replace("dog", "cat")
+print(res)
+
---
+++ day1prac2.py
@@ -0,0 +1,3 @@
+str1 = str(input("Enter String: "))
+print(str1.upper())
+print(f"Total number of characters: {len(str1)}")
